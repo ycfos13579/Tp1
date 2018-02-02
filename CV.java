66e2391199de7ebf2b1075d88e54806d8ff6dca7
YoucOuss <youcef-pino@hotmail.fr>
2018-02-02 11:16:28 -0500

    Méthodes ajoutées

--- CV.java
+++ CV.java
@@ -1,9 +1,36 @@
 
 public class CV {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String Nom;
+	private String Prénom;
+	private String Formation;
+	private int Expériences; 
+	private String [] Compétences; 
+	private String Attentes; 
 
+	public CV(String nom, String prénom, String formation, int expériences, String[] compétences, String attentes) {
+		super();
+		Nom = nom;
+		Prénom = prénom;
+		Formation = formation;
+		Expériences = expériences;
+		Compétences = compétences;
+		Attentes = attentes;
 	}
+	public void affiche() {
+		
+	}
+	public class Tests {
 
+		public void main(String[] args) {
+			
+			System.out.println("\nBienvenue chez Barette!");
+			
+			CV cv = new CV(Attentes, Attentes, Attentes, Expériences, args, Attentes);
+		}
+	}
+	
 }
+
+
+
